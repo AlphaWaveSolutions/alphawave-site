a7ca8977a8d96c2f33daf0cf7b5a0d182b229708
AlphaWaveSolutions <solutionsalphawave@gmail.com>
2026-01-29 16:44:45 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,7 @@
 // assets/config.js
+
 window.ALPHAWAVE_CONFIG = {
-  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbyvoJcPEb8pxi9cs1PpdaMLvvN0v8ndwZzb9fnRNshowejBP2Jo7o7QSq1pmZ6OOA/exec",
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbyoJcPEb8pxi9cs1PpdaMLvvN0v8ndwZzb9fnRNshovejBP2Jo7o7QSq1pmZ600A/exec",
   ADMIN_TOKEN: "aw_admin_9f3b1d6c2e7a4baf8a9d",
   BUSINESS: {
     name: "AlphaWave Solutions",
@@ -10,19 +11,3 @@ window.ALPHAWAVE_CONFIG = {
     email: "solutionsalphawave@gmail.com"
   }
 };
-  // Admin "hidden" access token (basic obscurity, not true security)
-  // Change this to a long random string.
- ADMIN_TOKEN: "aw_admin_9f3b1d6c2e7a4baf8a9d",
-
-  BUSINESS: {
-    name: "AlphaWave Solutions",
-    owner: "Leando Arthur Thys",
-    address: "16 King Street, Elim, Western Cape, South Africa",
-    phone: "+27 61 522 4124",
-    email: "solutionsalphawave@gmail.com"
-  }
-};
-
-
-
-
