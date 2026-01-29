a34d8cf1f91d7eeec78754e4ed818d4e15570559
AlphaWaveSolutions <solutionsalphawave@gmail.com>
2026-01-29 16:32:09 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 // assets/config.js
 window.ALPHAWAVE_CONFIG = {
-  GAS_ENDPOINT: "https://script.google.com/macros/s/XXXXX/exec",
+  GAS_ENDPOINT: "https://script.google.com/macros/library/d/1SLIfJsxp8vaserKcySq3MtNUgOw-yPnKh3D6rbsk5BY7zTTh-pZM5FdP/1",
   ADMIN_TOKEN: "aw_admin_9f3b1d6c2e7a4baf8a9d",
   BUSINESS: {
     name: "AlphaWave Solutions",
@@ -22,4 +22,5 @@ window.ALPHAWAVE_CONFIG = {
     email: "solutionsalphawave@gmail.com"
   }
 };
+
 
